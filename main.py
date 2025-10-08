2d99fe7d601c8962f4216bd3b360150622ca0dc8
anderseelmann-hub <ander.seelmann@tktk.ee>
2025-10-08 14:30:38 +0300

    add week 2 files

--- main.py
+++ main.py
@@ -1,9 +1,43 @@
-day = input("Mis päev on homme? (tööpäev/puhkepäev):")
+#Alusta programm
+#Küsi kasutajalt: "Mis päev on homme? Kas tööpäev või puhkepäev"
+#Salvesta vastus muutujasse day
+#Kui day on võrdne sõnaga "tööpäev", siis väljasta ekraanile: "Ma lähen magama, Head ööd!"
+#Muidu kui day on võrdne sõnaga "puhkepäev", siis väljasta ekraanile: "Veel üks osa Netflixist"
+#Muidu kui sisestus ei olnud õige, siis väljasta ekraanile: "Vale väärtus"
+#lõpeta programm
 
+""" day=input("Mis päev on homme? Kas tööpäev või puhkepäev: ")
 if day == "tööpäev":
-    print("Ma lähen magama, head ööd!:")
-
-elif day == "puhkpäev":
-    print("Veel üks osa Netflixist!")
+    print ("Ma lähen magama, Head ööd!")
+elif day == "puhkepäev":
+    print ("Veel üks osa Netflixist!")
 else:
-    print("Vale väärtus")
+    print ("Vale väärtus") """
+
+
+
+#Finantsnõustaja
+""" print("Tere tulemast programmi 'finantsnõustaja'!")
+print("Sinu isiklik nõusatja ei tee emotsionaalseid oste.")
+money = int (input ("Kui palju raha sul on praegu? "))
+if money < 2500:
+     print("Sul pole veel piisavalt raha, kogu edasi!")
+elif money == 2500:
+    print ("Palju õnne, saad osat endale uue telefoni!")
+ else:
+     print ("Saad osta uue telefoni ja raha jääb veel ülegi!") """
+
+
+#Sammulugeja
+""" goal=10000
+steps= int(input("Mitu sammu oled juba teinud? "))
+percent= (steps/goal) * 100
+print (f"{percent}%")
+if percent < 50:
+    print ("Alles poolel teel, liigub edasi!")
+elif percent < 75:
+    print ("Oled tubli, natuke veel!")
+elif percent < 100:
+    print ("Peaaegu kohal!")
+else:
+    print ("Oled eesmärgi täitnud, Õnnitlused!") """
