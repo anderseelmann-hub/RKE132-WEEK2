9b7e70412267a47af6cbcdf301200da7c09ef18c
anderseelmann-hub <ander.seelmann@tktk.ee>
2025-10-08 14:21:46 +0300

    add week 2 files

---
+++ main.py
@@ -0,0 +1,9 @@
+day = input("Mis päev on homme? (tööpäev/puhkepäev):")
+
+if day == "tööpäev":
+    print("Ma lähen magama, head ööd!:")
+
+elif day == "puhkpäev":
+    print("Veel üks osa Netflixist!")
+else:
+    print("Vale väärtus")
